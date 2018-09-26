05e23d7960b746845107025f61cb940ebaa73912
andrzejboni <andrzej.boni@outlook.com>
2018-09-26 15:34:13 +0200

    Poprawki do obsługi konsoli, dodanie opcji quit

--- src/SalaKonferencyjna/Main.java
+++ src/SalaKonferencyjna/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        SalaKonferencyjna s1 = new SalaKonferencyjna(100, false, 100, 30);
+        SalaKonferencyjna s1 = new SalaKonferencyjna(100, false, 80, 20);
         SalaKonferencyjna s2 = new SalaKonferencyjna(101, false, 150, 50);
         SalaKonferencyjna s3 = new SalaKonferencyjna(102, false, 200, 100);
         SalaKonferencyjna s4 = new SalaKonferencyjna(103, false, 300, 200);
-        SalaKonferencyjna s5 = new SalaKonferencyjna(104, false, 100, 30);
+        SalaKonferencyjna s5 = new SalaKonferencyjna(104, false, 100, 40);
 
         CentrumKonferencyjne centrumKonferencyjne = new CentrumKonferencyjne();
 
@@ -21,18 +21,18 @@ public class Main {
 
 //        System.out.println(centrumKonferencyjne.toString());
 //        centrumKonferencyjne.toString(); // Wypisuje wszystkie sale w objekcie i w ogóle i wszczególe.
-        System.out.println(centrumKonferencyjne.znajdzNajtanszaSale(130));
+//        System.out.println(centrumKonferencyjne.znajdzNajtanszaSale(130));
 
 
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.println("Wypisz staff który chcesz uzyskać: Działa: wypisz, znajdzNajtansze, znajdzNajtansza, zarezerwuj, zwolnij, wypiszStanSali");
-        int i=0;
-        while (i <=10) {
+        System.out.println("Wypisz staff który chcesz uzyskać, oferuję: \n wypiszWymaluj, znajdzNajtansze, znajdzNajtansza, zarezerwuj, zwolnij, wypiszStanSali, quit");
+        int i = 0;
+        while (i <= 100) {
             String tekst = sc.nextLine();
 
-            if (tekst.equals("wypisz")) {
+            if (tekst.equals("wypiszWymaluj")) {
                 System.out.println(centrumKonferencyjne.toString());
             }
 
@@ -56,7 +56,7 @@ public class Main {
             if (tekst.equals("zwolnij")) {
                 System.out.println("Podaj nr sali którą chcesz zwolnic: ");
                 String tekst2 = sc.nextLine();
-               System.out.println( centrumKonferencyjne.zwolnijSale(Integer.parseInt(tekst2)));
+                System.out.println(centrumKonferencyjne.zwolnijSale(Integer.parseInt(tekst2)));
             }
 
             if (tekst.equals("wypiszStanSali")) {
@@ -64,9 +64,12 @@ public class Main {
                 String tekst2 = sc.nextLine();
                 centrumKonferencyjne.wypiszStanSali(Integer.parseInt(tekst2));
             }
-i++;
+            if (tekst.equals("quit")) {
+                System.out.println("Dziękuje za skorzystanie z programu do rezerwacji sal.");
+                break;
+            }
+
+            i++;
         }
-
-
     }
 }
