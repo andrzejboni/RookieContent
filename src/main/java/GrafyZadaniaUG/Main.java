0ba5ee2770df492cd3054278b5a676a0c3afc2a5
andrzejboni <andrzej.boni@outlook.com>
2018-11-02 17:39:49 +0100

    Fabryka abstrakcyjna

--- src/main/java/GrafyZadaniaUG/Main.java
+++ src/main/java/GrafyZadaniaUG/Main.java
@@ -1,9 +1,7 @@
 package GrafyZadaniaUG;
 
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 
@@ -17,38 +15,59 @@ public class Main {
 
 //        System.out.println(utils.getLiczbaWierszyMacierzy());    // Works
 //        utils.addRowToFile("0 1 0 1 0");                          // Works
-//
-//        utils.readFromFile();
-//
-//        utils.wyswietlTablice();
+
 
         gui();
-
+        //  metoda wyswietl tablice nie aktualizuje grafu po dodaniu wierzcholka
+        // metoda dodaj wierzcholek dodaje jeden wierzcholek, po probie dodania kolejnego, nadpisuje stary
 
     }
 
     public static void gui() throws IOException {
-        System.out.println("Labolatorium 1.\n Macierze sąsiedstwa i ciągi graficzne, Algorytmiczna teoria grafów.");
+        System.out.println("\nLabolatorium 1. Macierze sąsiedstwa i ciągi graficzne, algorytmiczna teoria grafów.");
         Utils utils = new Utils();
 
         Scanner scanner = new Scanner(System.in);
         String zKlawiatury;
 
         do {
-            System.out.println("\nMenu: [1]wczytac graf z pliku [2]wyswietl tablice [3]quit");
+            System.out.println("Menu:\n" +
+                    "[1]wczytaj tablice         [4]dodaj wierzcholek        [7]stopnie wierzchołka \n" +
+                    "[2]wyswietl tablice        [5]modyfikuj krawędź        [99]quit\n" +
+                    "[3]liczba wierzcholków     [6]wyczysc macierz\n" +
+                    " ");
             zKlawiatury = scanner.nextLine();
 
             if (zKlawiatury.equals("1")) {
                 utils.readFromFile();
             }
-
-
             if (zKlawiatury.equals("2")) {
                 utils.wyswietlTablice();
-
+            }
+            if (zKlawiatury.equals("3")) {
+                utils.getLiczbaWierzcholkow();
+            }
+            if (zKlawiatury.equals("4")) {
+                utils.dodajWierzcholek();
+            }
+            if(zKlawiatury.equals("6")){
+                utils.wyczyscMacierz();
+            }
+            if(zKlawiatury.equals("7")){
+                utils.stopnieWierzcholka();
+            }
+            if(zKlawiatury.equals("5")){
+                utils.modyfikujKrawedz();
+            }
+            if(zKlawiatury.equals("5")){
+                utils.modyfikujKrawedz();
+            }
+            if(zKlawiatury.equals("5")){
+                utils.modyfikujKrawedz();
             }
 
-        } while (!zKlawiatury.equals("3"));
+
+        } while (!zKlawiatury.equals("99"));
 
     }
 
--- src/main/java/GrafyZadaniaUG/Utils.java
+++ src/main/java/GrafyZadaniaUG/Utils.java
@@ -1,32 +1,25 @@
 package GrafyZadaniaUG;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Utils {
 
     String file = "C:\\git\\Rookie77\\src\\main\\java\\GrafyZadaniaUG\\graf";
-    public static int liczbaWierszyMacierzy;
+    public static int liczbaWierzcholkow;
 
-    public static int[][] macierzGrafu = new int[Utils.liczbaWierszyMacierzy + 20][Utils.liczbaWierszyMacierzy + 20];
-
+    public static int[][] macierzGrafu = new int[Utils.liczbaWierzcholkow + 50][Utils.liczbaWierzcholkow + 50];
 
     // pierwsza linia oznacza ilosc wierzcholkow czy tam krawedzi
 // biblioteka matrix do mnozenia macierzy albo cos
 
 
-
-
-
-    public int getLiczbaWierszyMacierzy() throws IOException { // Tested works
-        try (Scanner br = new Scanner(new File(file))) {
-            liczbaWierszyMacierzy = br.nextInt();
-        }
-            return liczbaWierszyMacierzy;
+    public void getLiczbaWierzcholkow() {
+        System.out.println("Liczba wierzchołków: " + liczbaWierzcholkow);
     }
 
-    public void addRowToFile(String row) throws IOException { //  Tested Works
+    public void addRowToFile(String row) throws IOException { //  Tested Works - dopisuje na koncu pliku jedna linie
         FileWriter writer = new FileWriter(file, true);
         writer.write("\n"); // zapisywanie Wiersza odbywa sie w nowej linii !!!!
         writer.write(row);
@@ -36,10 +29,10 @@ public class Utils {
     public void readFromFile() throws IOException {    // Zapisuje do tablicy a nastepnie
         try (Scanner br = new Scanner(new File(file))) {
             int line;
-            liczbaWierszyMacierzy = br.nextInt();
+            liczbaWierzcholkow = br.nextInt();
 
-            for (int i = 0; i < liczbaWierszyMacierzy; i++) { // Liczba wierszy jest równa liczbie kolumn
-                for (int j = 0; j < liczbaWierszyMacierzy; j++) {
+            for (int i = 0; i < liczbaWierzcholkow; i++) { // Liczba wierszy jest równa liczbie kolumn
+                for (int j = 0; j < liczbaWierzcholkow; j++) {
                     macierzGrafu[i][j] = br.nextInt();
                 }
             }
@@ -47,89 +40,65 @@ public class Utils {
     }
 
     public void wyswietlTablice() {
-        for (int i = 0; i < liczbaWierszyMacierzy; i++) {
+        for (int i = 0; i < liczbaWierzcholkow; i++) {
             System.out.println("\n");
-            for (int j = 0; j < liczbaWierszyMacierzy; j++) {
+            for (int j = 0; j < liczbaWierzcholkow; j++) {
                 System.out.printf(" " + macierzGrafu[i][j]);
             }
         }
-
-
     }
 
-
-
-
-    public void dodajWierzcholekRoBOCZANAZWA() { // po przekątnej muszą być zawsze same zera -> wierzchołek nie może być połączony sam ze sobą
+    public void dodajWierzcholek() { // po przekątnej muszą być zawsze same zera -> wierzchołek nie może być połączony sam ze sobą
         // Dodając wierzchołek, powiekszamy zarówno liczbę wierszy i liczbę kolumn w pliku <- tak to ma działać.
 
-        liczbaWierszyMacierzy = liczbaWierszyMacierzy + 1;
+        System.out.println("Wpisz połączenia wierzchołka pionowo \nDodajesz:" + liczbaWierzcholkow + 1 + "wierzchołek."); // 1 2 3 .. n  gdzie n to liczba krawedzi
 
-        for (int i = 0; i < liczbaWierszyMacierzy; i++) {
+//        liczbaWierzcholkow = liczbaWierzcholkow+1; // TRZEBA DODAC ZMIANE LICZBY OZNACZAJACEJ LICZBE KRAWEDZI! TO NIE ZAPISUJE SI EDO TABLICY
 
+        Scanner zKlawiatury = new Scanner(System.in);
+        String linia = zKlawiatury.nextLine();  // wpisuje mu linie
+        String[] tablicaZnakowPionow = linia.split(" "); // robie z tego tablice, spoko
 
-            System.out.println("\nDodawany wierzchołek ma być połączony z wierzchołkiem? Wpisz 1 lub 0"); // Zamienic to na dodawnanie automatycznie
-            // to znaczy podaje w ciągu np. 1 1 0 1 1 - > i on automatiko wie, co gdzie i ile ma wypełnić
-            Scanner zKlawiatury = new Scanner(System.in);
-            int klaw = zKlawiatury.nextInt();
-
-            if (klaw == 1) {
-                macierzGrafu[i][liczbaWierszyMacierzy] = 1;
-            } else
-                macierzGrafu[i][liczbaWierszyMacierzy] = 0;
-            }
-
-
-
+        for (int i = 0; i < liczbaWierzcholkow; i++) {
+            macierzGrafu[i][liczbaWierzcholkow] = Integer.parseInt(tablicaZnakowPionow[i]);
         }
 
+        System.out.println("\nWpisz połączenia wierzchołka poziomo \nDodajesz:" + liczbaWierzcholkow + 1 + "wierzchołek.");
+        linia = zKlawiatury.nextLine();  // wpisuje mu linie
+        String tablicaZnakowPoziomo[] = linia.split(" "); // robie z tego tablice, spoko
 
-
-        // Dodać podloge
-
-
-
-    // Teraz należy
-// usunąć wszystko z pliku i zapisac tam jeszcze raz z macierzy.
-
-
-
-
-
-    public void addTopToTheFile() {
-
-
-    }
-//public static int[][] macierzGrafu = new int[Utils.liczbaWierszyMacierzy+20][Utils.liczbaWierszyMacierzy+20];
-
-
-    public void removeRowFromFile(int whichRow) { // Najpierw trzeba policzyc ile mam wierszy w pliwkersie a następnie przeiterować po inich
-        // i zamienić dany wiersz na 0;
-//
-//        FileWriter writer = new FileWriter(file, true);
-//
-//
-//        writer.write("");
-//
-//        writer.close();
-
+        for (int i = 0; i < liczbaWierzcholkow; i++) {
+            macierzGrafu[liczbaWierzcholkow][i] = Integer.parseInt(tablicaZnakowPoziomo[i]);
+        }
+        liczbaWierzcholkow += 1;
     }
 
 
-    public void removeAllFromFile() throws IOException {
+    public void modyfikujKrawedz() {
+
+        System.out.println("Podaj ciąg znaków do edytowania krawędzi, wzór: x y 1 ");
+        Scanner zKlawiatury = new Scanner(System.in);
+        String linia = zKlawiatury.nextLine();  // wpisuje mu linie
+        String[] tablicaZnakowPionow = linia.split(" "); // robie z tego tablice, spoko
+        macierzGrafu[Integer.parseInt(tablicaZnakowPionow[0])][Integer.parseInt(tablicaZnakowPionow[1])] = Integer.parseInt(tablicaZnakowPionow[2]);
+        System.out.println("Zmodyfikowano krawędź");
+    }
+
+
+    public void wyczyscPlik() throws IOException {
 
         PrintWriter writer = new PrintWriter(file);
-
-
         writer.print("");
-
         writer.close();
+    }
 
-
-
-//        PrintWriter pw = new PrintWriter(file);
-//        pw.close();
-
+    public void wyczyscMacierz() throws IOException {
+        for (int i = 0; i < liczbaWierzcholkow; i++) {
+            System.out.println("\n");
+            for (int j = 0; j < liczbaWierzcholkow; j++) {
+                macierzGrafu[i][j] = 0;
+            }
+        }
     }
 
     public void writeToFile(String column) throws IOException {
@@ -139,33 +108,30 @@ public class Utils {
         writer.close();
     }
 
-    public void dodajKrawedz(String line){
+    public void stopnieWierzcholka() {
 
-    }
-    public void usunKrawedz(){
+        List<Integer> stopien = new ArrayList<>();
+        
+        for (int i = 0; i < liczbaWierzcholkow; i++) {
+            stopien.add(i, 0);
+            for (int j = 0; j < liczbaWierzcholkow; j++) {
 
-    }
-    public void dodajWierzcholek(){
-
-    }
-    public void usnunWierzcholek(){
-
-    }
-
-    public void wyznaczStopienWierzcholka(){
-
-    }
-    public void maksymalnyStopienGrafu(){
-
-    }
-    public void iloscWierzcholkowParzystych(){
-
+                if (macierzGrafu[i][j] == 1) {
+                    stopien.set(i, stopien.get(i) + 1);
+                }
+            }
+        }
+        System.out.println("\n");
+        System.out.println("Maksymalny stopień grafu to " + Collections.max(stopien)
+                + ", minimalny: " + Collections.min(stopien));
+        for (int i = 0; i < liczbaWierzcholkow; i++) {
+            System.out.println("Stopień wierzchołka " + i + " wynosi: " + stopien.get(i));
+        }
     }
 
-    public void iloscWierzcholkowNieparzystych(){
+    public void parzystoscWierzcholkow() {
 
     }
 
 
 }
-//
